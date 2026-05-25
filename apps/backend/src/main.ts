3eac17e6f0cf5136d924b3ca7d3097e8138cfe39
Hairum Qureshi <hqureshi@udel.edu>
2026-05-25 19:19:12 -0400

    configured global validation pipe

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,11 +4,14 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
     origin: configService.get<string>('FRONTEND_URL'),
