7b00e2a61e94cfca4090cc1d8a5a83c5d1c03ab7
Hairum Qureshi <hqureshi@udel.edu>
2026-05-25 19:18:58 -0400

    created DTOs folder with a sign up DTO inside

---
+++ apps/backend/src/DTOs/signUp.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class SignUpDto {
+  @IsNotEmpty()
+  @IsEmail()
+  email!: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  username!: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  password!: string;
+}
