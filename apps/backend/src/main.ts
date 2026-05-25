751810b6be5c0e176c04847818403fb76f79d087
Hairum Qureshi <159098695+Hairum-Qureshi@users.noreply.github.com>
2026-05-25 16:12:31 -0400

    Initial commit

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
+  
+  await app.listen(process.env.PORT ?? 3000);
+  console.log('Nest.js Server successfully started');
+}
+bootstrap();
