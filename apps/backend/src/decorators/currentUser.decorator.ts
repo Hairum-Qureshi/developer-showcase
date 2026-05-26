0389aeb6d961e8894aa826d02a2ab504ed757381
Hairum Qureshi <hqureshi@udel.edu>
2026-05-26 13:41:18 -0400

    updated userPayload object so it correctly gets all the user data

---
+++ apps/backend/src/decorators/currentUser.decorator.ts
@@ -0,0 +1,20 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { AuthRequest } from '../types';
+
+export const CurrentUser = createParamDecorator(
+  (_: unknown, ctx: ExecutionContext) => {
+    const request: AuthRequest = ctx.switchToHttp().getRequest();
+    const user = request.user;
+    if (!user) return null;
+
+    const userPayload = {
+      user_id: user.user_id,
+      username: user.username,
+      profilePictureSeed: `https://api.dicebear.com/9.x/identicon/svg?seed=${user.profilePictureSeed}`,
+      email: user.email,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+    return userPayload;
+  },
+);
