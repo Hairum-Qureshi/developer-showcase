c4d133bf45a1f250f23b7e5c2a5299fc4356ce6a
Hairum Qureshi <hqureshi@udel.edu>
2026-05-26 02:38:48 -0400

    added logic to show the sign up/sign in form depending on what the user chooses

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+import SignIn from "../components/SignIn";
+import SignUp from "../components/SignUp";
+
 export default function Home() {
-	return <div>Home</div>;
+  const [showSignUp, setShowSignUp] = useState(true);
+  const [_, setShowSignIn] = useState(false);
+
+  return (
+    <main className="min-h-screen bg-gradient-to-br from-gray-950 via-gray-900 to-black flex items-center justify-center px-6">
+      <div className="w-full max-w-md">
+        {showSignUp ? (
+          <SignUp setShowSignUp={setShowSignUp} setShowSignIn={setShowSignIn} />
+        ) : (
+          <SignIn setShowSignUp={setShowSignUp} setShowSignIn={setShowSignIn} />
+        )}
+      </div>
+    </main>
+  );
 }
