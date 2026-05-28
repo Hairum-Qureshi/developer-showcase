341bd312dc68d6ef0d630cf81dc9916e8872e53e
Hairum Qureshi <hqureshi@udel.edu>
2026-05-28 18:40:20 -0400

    added missing biography property to UserPayload type

--- apps/backend/src/types.ts
+++ apps/backend/src/types.ts
@@ -2,6 +2,7 @@ type UserPayload = {
   user_id: string;
   email: string;
   username: string;
+  biography: string;
   profilePictureSeed: string;
   twitter_x_url: string;
   linkedin_url: string;
