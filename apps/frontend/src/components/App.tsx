bfa169daa7041e32de8de113aed7d35baf8739ca
Hairum Qureshi <hqureshi@udel.edu>
2026-05-28 17:51:29 -0400

    added feed route

--- apps/frontend/src/components/App.tsx
+++ apps/frontend/src/components/App.tsx
@@ -5,6 +5,7 @@ import Home from "../pages/Home";
 import About from "../pages/About";
 import NotFound from "../pages/NotFound";
 import Profile from "../pages/Profile";
+import Feed from "../pages/Feed";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile/:userId" element={<Profile />} />
+        <Route path="/feed" element={<Feed />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
