a201dd3c017124981816489ac2d550aee612dcfb
Hairum Qureshi <hqureshi@udel.edu>
2026-05-29 17:58:28 -0400

    changed 'api/auth' to just 'auth' after setting global 'api' prefix; also fixed cookie logic so it properly gets set

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import type { Response, CookieOptions } from 'express';
 import * as types from '../types';
 import { AuthService } from './auth.service';
@@ -6,37 +6,38 @@ import { CurrentUser } from '../decorators/currentUser.decorator';
 import { SignUpDto } from '../DTOs/signUp.dto';
 import { SignInDto } from '../DTOs/signIn.dto';
 import express from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
-@Controller('api/auth')
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   private createCookie(res: Response, jwtToken: string) {
-    res.cookie('auth-session', jwtToken, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
-    });
+    res.cookie(
+      'auth-session',
+      jwtToken,
+      this.authService.getAuthCookieOptions() as CookieOptions,
+    );
   }
 
   @Post('sign-up')
   async signUp(
     @Body() signUpDto: SignUpDto,
     @Res({ passthrough: true }) res: express.Response,
-  ): Promise<{ jwtToken: string }> {
-    const { jwtToken } = await this.authService.signUp(signUpDto);
+  ): Promise<{ jwtToken: string; user_id: string }> {
+    const { jwtToken, user_id } = await this.authService.signUp(signUpDto);
     this.createCookie(res, jwtToken);
-    return { jwtToken };
+    return { jwtToken, user_id };
   }
 
   @Post('sign-in')
   async signIn(
     @Body() signInDto: SignInDto,
     @Res({ passthrough: true }) res: express.Response,
-  ): Promise<{ jwtToken: string }> {
-    const { jwtToken } = await this.authService.signIn(signInDto);
+  ): Promise<{ jwtToken: string; user_id: string }> {
+    const { jwtToken, user_id } = await this.authService.signIn(signInDto);
     this.createCookie(res, jwtToken);
-    return { jwtToken };
+    return { jwtToken, user_id };
   }
 
   @Post('sign-out')
@@ -51,7 +52,7 @@ export class AuthController {
   }
 
   @Get('current-user')
-  //   @UseGuards(AuthGuard())
+  @UseGuards(AuthGuard())
   getCurrentUser(@CurrentUser() user: types.UserPayload): types.UserPayload {
     return this.authService.getCurrentUser(user);
   }
