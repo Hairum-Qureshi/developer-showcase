7be2c93b982868456fdb6e87100b98725d9b8093
Hairum Qureshi <hqureshi@udel.edu>
2026-05-29 17:55:23 -0400

    fixed module file so it does not contain API endpoint

--- apps/backend/src/profile/profile.module.ts
+++ apps/backend/src/profile/profile.module.ts
@@ -1,26 +1,12 @@
-import { Body, Module, Patch, UseGuards } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
 import { PostgresDbModule } from '../postgres-db.module';
 import { AuthModule } from '../auth/auth.module';
-import * as types from '../types';
-import { CurrentUser } from '../decorators/currentUser.decorator';
-import { AuthGuard } from '@nestjs/passport';
 
 @Module({
   imports: [PostgresDbModule, AuthModule],
   providers: [ProfileService],
   controllers: [ProfileController],
 })
-export class ProfileModule {
-  constructor(private readonly profileService: ProfileService) {}
-
-  @Patch('update-biography')
-  @UseGuards(AuthGuard())
-  async updateBiography(
-    @CurrentUser() user: types.UserPayload,
-    @Body('biography') biography: string,
-  ) {
-    return this.profileService.updateBiography(user.user_id, biography);
-  }
-}
+export class ProfileModule {}
