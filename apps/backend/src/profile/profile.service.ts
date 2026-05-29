5ebf9d8f876ffa33306bf5194aaa7ac4ae001646
Hairum Qureshi <hqureshi@udel.edu>
2026-05-29 00:27:46 -0400

    created service method for updating the user's biography as well as ensuring it's sanitized

--- apps/backend/src/profile/profile.service.ts
+++ apps/backend/src/profile/profile.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Inject, Injectable } from '@nestjs/common';
+import { sanitize } from '@markdown-design/markdown-it-sanitize';
+import markdownit from 'markdown-it';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  constructor(@Inject('POSTGRES_POOL') private readonly sql: any) {}
+
+  async updateBiography(userId: string, biography: string) {
+    if (!biography.trim().length)
+      throw new HttpException('Biography cannot be empty', 400);
+
+    const markdown = markdownit({ html: true });
+    markdown.use(sanitize);
+    const sanitizedBiography = markdown.render(biography);
+
+    await this.sql`
+      UPDATE users
+      SET biography = ${sanitizedBiography}
+      WHERE user_id = ${userId}
+    `;
+
+    return { message: 'Biography updated successfully' };
+  }
+}
