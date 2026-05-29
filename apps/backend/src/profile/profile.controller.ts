80a4f1eca85bf2bc13df9fe8d090ce49e5b1d5bf
Hairum Qureshi <hqureshi@udel.edu>
2026-05-29 17:55:39 -0400

    added update biography PATCH endpoint

--- apps/backend/src/profile/profile.controller.ts
+++ apps/backend/src/profile/profile.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Patch, UseGuards } from '@nestjs/common';
+import { ProfileService } from './profile.service';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUser } from '../decorators/currentUser.decorator';
+import type { UserPayload } from '../types';
 
 @Controller('profile')
-export class ProfileController {}
+export class ProfileController {
+  constructor(private readonly profileService: ProfileService) {}
+
+  @Patch('update-biography')
+  @UseGuards(AuthGuard())
+  async updateBiography(
+    @CurrentUser() user: UserPayload,
+    @Body('biography') biography: string,
+  ) {
+    return this.profileService.updateBiography(user.user_id, biography);
+  }
+}
