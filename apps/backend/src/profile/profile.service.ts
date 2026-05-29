5696eaac3014c30e37ebf3eb07db499a44549961
Hairum Qureshi <hqureshi@udel.edu>
2026-05-29 18:29:51 -0400

    fixed markdown sanitization logic so it doesn't convert the markdown to HTML

--- apps/backend/src/profile/profile.service.ts
+++ apps/backend/src/profile/profile.service.ts
@@ -1,6 +1,5 @@
 import { HttpException, Inject, Injectable } from '@nestjs/common';
-import { sanitize } from '@markdown-design/markdown-it-sanitize';
-import markdownit from 'markdown-it';
+import DOMPurify from 'isomorphic-dompurify';
 
 @Injectable()
 export class ProfileService {
@@ -10,43 +9,9 @@ export class ProfileService {
     if (!biography.trim().length)
       throw new HttpException('Biography cannot be empty', 400);
 
-    const markdown = markdownit({ html: true });
-    markdown.use(sanitize, {
-      ALLOWED_TAGS: [
-        'p',
-        '#text',
-        'br', // Text structure
-        'strong',
-        'em',
-        'del',
-        's', // Text emphasis
-        'h1',
-        'h2',
-        'h3',
-        'h4',
-        'h5',
-        'h6', // Headings
-        'ul',
-        'ol',
-        'li', // Lists
-        'a', // Links
-        'img', // Images
-        'blockquote',
-        'pre',
-        'code', // Blocks & Code
-        'table',
-        'thead',
-        'tbody',
-        'tr',
-        'th',
-        'td', // Tables
-      ],
-      ALLOWED_ATTRIBUTES: {
-        a: ['href', 'title', 'target'], // Crucial for links
-        img: ['src', 'alt', 'title', 'width', 'height'], // Crucial for images
-      },
+    const sanitizedBiography = DOMPurify.sanitize(biography, {
+      FORCE_BODY: true,
     });
-    const sanitizedBiography = markdown.render(biography);
 
     await this.sql`
       UPDATE users
