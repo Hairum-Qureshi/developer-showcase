a474f032712786ad0c9d08f4fb612fc4cde4f987
Hairum Qureshi <hqureshi@udel.edu>
2026-06-01 07:33:08 -0400

    added Global decorator to Auth module to make it globally accessible

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PostgresDbModule } from '../postgres-db.module';
@@ -7,6 +7,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './jwt-strategy';
 
+@Global()
 @Module({
   imports: [
     PostgresDbModule,
