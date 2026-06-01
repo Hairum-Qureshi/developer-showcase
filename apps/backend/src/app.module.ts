bed00c00c660b16535435a8b1d5143eb88626555
Hairum Qureshi <hqureshi@udel.edu>
2026-06-01 08:03:39 -0400

    added missing AuthModule and ProfileModuel to imports array

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
+    ProfileModule,
   ],
   controllers: [AppController],
   providers: [AppService],
