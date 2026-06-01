ff3253b87d75c2e1cb3a60f0aa9e44fb4e77b5ef
Hairum Qureshi <hqureshi@udel.edu>
2026-06-01 13:14:06 -0400

    updated URL to include scope as well

--- apps/frontend/src/components/GitHubAuthButton.tsx
+++ apps/frontend/src/components/GitHubAuthButton.tsx
@@ -2,7 +2,15 @@ import { FaGithub } from "react-icons/fa";
 
 export default function GitHubAuthButton() {
   return (
-    <button className="w-full rounded-xl bg-gray-700 py-3 text-white transition hover:bg-gray-600 active:scale-[0.99] flex items-center justify-center gap-2 hover:cursor-pointer">
+    <button
+      className="w-full rounded-xl bg-gray-700 py-3 text-white transition hover:bg-gray-600 active:scale-[0.99] flex items-center justify-center gap-2 hover:cursor-pointer"
+      onClick={(e) => {
+        e.preventDefault();
+        window.location.assign(
+          `https://www.github.com/login/oauth/authorize?client_id=${import.meta.env.VITE_GITHUB_CLIENT_ID}&scope=user:email`,
+        );
+      }}
+    >
       <FaGithub className="text-xl" />
       Login With GitHub
     </button>
