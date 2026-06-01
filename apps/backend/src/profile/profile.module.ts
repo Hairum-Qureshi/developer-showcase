b251f6502c7af8ae3262515227de1a039af4463c
Hairum Qureshi <hqureshi@udel.edu>
2026-06-01 08:03:17 -0400

    removed AuthModule from imports array since it's now global

--- apps/backend/src/profile/profile.module.ts
+++ apps/backend/src/profile/profile.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
 import { PostgresDbModule } from '../postgres-db.module';
-import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PostgresDbModule, AuthModule],
+  imports: [PostgresDbModule],
   providers: [ProfileService],
   controllers: [ProfileController],
 })
