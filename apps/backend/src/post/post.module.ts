cf39750df2132172c336df9c99e10c9731f5ee7d
Hairum Qureshi <hqureshi@udel.edu>
2026-06-02 10:36:37 -0400

    imported PostgresDBModule

--- apps/backend/src/post/post.module.ts
+++ apps/backend/src/post/post.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { PostgresDbModule } from '../postgres-db.module';
 
 @Module({
+  imports: [PostgresDbModule],
   providers: [PostService],
-  controllers: [PostController]
+  controllers: [PostController],
 })
 export class PostModule {}
