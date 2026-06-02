0c618fb5a7929bee0ce19711a30e549d9830855d
Hairum Qureshi <hqureshi@udel.edu>
2026-06-02 15:15:43 -0400

    imported UploadCareModule

--- apps/backend/src/post/post.module.ts
+++ apps/backend/src/post/post.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { PostgresDbModule } from '../postgres-db.module';
+import { UploadCareModule } from '../upload-care/upload-care.module';
 
 @Module({
-  imports: [PostgresDbModule],
+  imports: [PostgresDbModule, UploadCareModule],
   providers: [PostService],
   controllers: [PostController],
 })
