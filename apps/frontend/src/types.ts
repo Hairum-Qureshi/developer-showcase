4fe6f079e28f5745926daf0d65c64bdac9b33b3a
Hairum Qureshi <hqureshi@udel.edu>
2026-06-03 17:37:04 -0400

    created PostType

---
+++ apps/frontend/src/types.ts
@@ -0,0 +1,12 @@
+type PostType = {
+  post_id: string;
+  thumbnail_url: string;
+  title: string;
+  description: string;
+  github_link: string;
+  live_demo_link: string;
+  tags: string[];
+  created_at: string;
+};
+
+export type { PostType };
