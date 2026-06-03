c93d5b030abd55d9cd62491063a74c871a8cc532
Hairum Qureshi <hqureshi@udel.edu>
2026-06-03 15:54:17 -0400

    added route to view all users

--- apps/frontend/src/components/App.tsx
+++ apps/frontend/src/components/App.tsx
@@ -7,6 +7,7 @@ import PostForm from "../pages/PostForm";
 import PostContent from "../pages/PostContent";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
+import AllUsers from "../pages/AllUsers";
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
         <Route path="/profile/:userId" element={<Profile />} />
         <Route path="/login/callback" element={<Home />} />
         <Route path="/post/:postId" element={<PostContent />} />
+        <Route path="/users" element={<AllUsers />} />
         <Route path="/feed" element={<Feed />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
