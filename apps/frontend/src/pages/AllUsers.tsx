8ec61652bfc8d186dab33ecd64da836595aa63c3
Hairum Qureshi <hqureshi@udel.edu>
2026-06-03 15:54:31 -0400

    added basic styling for all users page

---
+++ apps/frontend/src/pages/AllUsers.tsx
@@ -0,0 +1,9 @@
+export default function AllUsers() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-black via-slate-950 to-slate-900 max-h-auto">
+      <div className="p-20">
+        <h1 className="font-bold text-white">All Users</h1>
+      </div>
+    </div>
+  );
+}
