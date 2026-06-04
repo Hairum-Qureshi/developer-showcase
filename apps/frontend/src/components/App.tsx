00a746c64ca66d3e08e3331437f405b8272bc42a
Hairum Qureshi <hqureshi@udel.edu>
2026-06-04 16:59:47 -0400

    made it so that the Navbar is only shown on specific routes

--- apps/frontend/src/components/App.tsx
+++ apps/frontend/src/components/App.tsx
@@ -1,27 +1,28 @@
-import "../css/index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Profile from "../pages/Profile";
 import Feed from "../pages/Feed";
-import Navbar from "./Navbar";
 import PostForm from "../pages/PostForm";
 import PostContent from "../pages/PostContent";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
 import AllUsers from "../pages/AllUsers";
+import NavbarLayout from "./NavbarLayout";
+import "../css/index.css";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
+        <Route element={<NavbarLayout />}>
+          <Route path="/new-post" element={<PostForm />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+          <Route path="/post/:postId" element={<PostContent />} />
+          <Route path="/users" element={<AllUsers />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
         <Route path="/" element={<Home />} />
-        <Route path="/new-post" element={<PostForm />} />
-        <Route path="/profile/:userId" element={<Profile />} />
         <Route path="/login/callback" element={<Home />} />
-        <Route path="/post/:postId" element={<PostContent />} />
-        <Route path="/users" element={<AllUsers />} />
-        <Route path="/feed" element={<Feed />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
