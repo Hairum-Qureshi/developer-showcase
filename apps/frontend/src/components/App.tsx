f1b0be29d0f7bfc32419358dd8de6c7ff822513d
Hairum Qureshi <hqureshi@udel.edu>
2026-06-04 17:05:52 -0400

    wrapped certain routes with ProtectedRoutesGuard middleware guard component that can only be accessed if you're authenticated

--- apps/frontend/src/components/App.tsx
+++ apps/frontend/src/components/App.tsx
@@ -8,21 +8,50 @@ import NotFound from "../pages/NotFound";
 import AllUsers from "../pages/AllUsers";
 import NavbarLayout from "./NavbarLayout";
 import "../css/index.css";
+import ProtectedRoutesGuard from "./ProtectedRoutesGuard";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<NavbarLayout />}>
-          <Route path="/new-post" element={<PostForm />} />
-          <Route path="/profile/:userId" element={<Profile />} />
+          <Route
+            path="/new-post"
+            element={
+              <ProtectedRoutesGuard>
+                <PostForm />
+              </ProtectedRoutesGuard>
+            }
+          />
+          <Route
+            path="/profile/:userId"
+            element={
+              <ProtectedRoutesGuard>
+                <Profile />
+              </ProtectedRoutesGuard>
+            }
+          />
           <Route path="/post/:postId" element={<PostContent />} />
-          <Route path="/users" element={<AllUsers />} />
+          <Route
+            path="/users"
+            element={
+              <ProtectedRoutesGuard>
+                <AllUsers />
+              </ProtectedRoutesGuard>
+            }
+          />
           <Route path="/feed" element={<Feed />} />
           <Route path="*" element={<NotFound />} />
         </Route>
         <Route path="/" element={<Home />} />
-        <Route path="/login/callback" element={<Home />} />
+        <Route
+          path="/login/callback"
+          element={
+            <ProtectedRoutesGuard>
+              <Home />
+            </ProtectedRoutesGuard>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
