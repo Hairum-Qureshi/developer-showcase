2a69f87b8954194c23f826232b9c6ddceac398d5
Hairum Qureshi <hqureshi@udel.edu>
2026-06-03 22:28:35 -0400

    imported HttpModule for Nest Axios request

--- apps/backend/src/post/post.module.ts
+++ apps/backend/src/post/post.module.ts
@@ -3,9 +3,10 @@ import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { PostgresDbModule } from '../postgres-db.module';
 import { UploadCareModule } from '../upload-care/upload-care.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [PostgresDbModule, UploadCareModule],
+  imports: [PostgresDbModule, UploadCareModule, HttpModule],
   providers: [PostService],
   controllers: [PostController],
 })
