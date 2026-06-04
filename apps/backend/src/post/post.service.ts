56589eb3583b7a7c42a400a80c217744f8f4ebc6
Hairum Qureshi <hqureshi@udel.edu>
2026-06-04 10:59:25 -0400

    fixed logic for extracting the file UUID from URL

--- apps/backend/src/post/post.service.ts
+++ apps/backend/src/post/post.service.ts
@@ -55,6 +55,7 @@ export class PostService {
     });
 
     const postID = snowflake.generate();
+
     let thumbnailURL: string | undefined = undefined;
     const slideShowURLs: string[] = [];
 
@@ -148,9 +149,9 @@ export class PostService {
 
     for (const url of imageURLs) {
       if (url) {
-        const fileID = url.split('/').pop()?.split('.')[0];
+        const fileID = url.split('/')[3];
         if (fileID) {
-          await firstValueFrom(
+          const response = await firstValueFrom(
             this.httpService.delete(
               `https://api.uploadcare.com/files/${fileID}/storage/`,
               {
@@ -161,6 +162,8 @@ export class PostService {
               },
             ),
           );
+
+          console.log('File deletion response:', response.data);
         }
       }
     }
