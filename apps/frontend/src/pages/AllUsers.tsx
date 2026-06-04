b89d90c507dc8995a6eda23937b7919f8ae59fb8
Hairum Qureshi <hqureshi@udel.edu>
2026-06-03 20:11:21 -0400

    added basic styling to all users page and logic to conditionally render UserCard components

--- apps/frontend/src/pages/AllUsers.tsx
+++ apps/frontend/src/pages/AllUsers.tsx
@@ -1,8 +1,29 @@
+import UserCard from "../components/UserCard";
+import useUsers from "../hooks/useUsers";
+import type { UserType } from "../types";
+
 export default function AllUsers() {
+  const { users } = useUsers();
   return (
     <div className="min-h-screen bg-gradient-to-br from-black via-slate-950 to-slate-900 max-h-auto">
-      <div className="p-20">
+      <div className="m-auto w-2/3 py-25 text-slate-50">
         <h1 className="font-bold text-white">All Users</h1>
+        <div className="space-y-2 my-4">
+          {users?.map((user: UserType) => {
+            return (
+              <UserCard
+                key={user.user_id}
+                profilePicture={
+                  user.avatar
+                    ? user.avatar
+                    : `https://api.dicebear.com/9.x/identicon/svg?seed=${user.profile_picture_seed}`
+                }
+                username={user.username}
+                joinedDate={user.created_at}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
