24dd5c975518098886e3e10848e49711be488e37
Hairum Qureshi <hqureshi@udel.edu>
2026-06-04 22:23:18 -0400

    styled 404 page

--- apps/frontend/src/pages/NotFound.tsx
+++ apps/frontend/src/pages/NotFound.tsx
@@ -1,3 +1,8 @@
 export default function NotFound() {
-	return <div>NotFound</div>;
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-black via-slate-950 to-slate-900 max-h-auto flex items-center justify-center flex-col">
+      <h1 className="text-6xl font-bold text-slate-600">404</h1>
+      <p className="text-xl text-slate-400 mt-2">Page Not Found</p>
+    </div>
+  );
 }
