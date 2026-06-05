e3f8257739225bd2e14ade3f60d798c004326ab5
Hairum Qureshi <hqureshi@udel.edu>
2026-06-04 21:24:36 -0400

    moved logic from Home component to Auth component

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,57 +1,5 @@
-import { useState } from "react";
-import SignIn from "../components/SignIn";
-import SignUp from "../components/SignUp";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import axios from "axios";
-import { useEffect, useRef } from "react";
-
 export default function Home() {
-  const [showSignUp, setShowSignUp] = useState(true);
-  const [_, setShowSignIn] = useState(false);
-
-  const [searchParams] = useSearchParams();
-  const effectRan = useRef(false);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const code = searchParams.get("code");
-
-    if (!code) return;
-
-    if (!effectRan.current) {
-      const fetchGitHubAccessToken = async () => {
-        const response = await axios.get(
-          `${import.meta.env.VITE_BACKEND_URL}/api/auth/github/access-token?code=${code}`,
-        );
-
-        const res = await axios.post(
-          `${import.meta.env.VITE_BACKEND_URL}/api/auth/github/authenticate`,
-          {},
-          {
-            headers: {
-              Authorization: `Bearer ${response.data}`,
-            },
-            withCredentials: true,
-          },
-        );
-
-        navigate(`/profile/${res.data.user_id}`);
-      };
-
-      fetchGitHubAccessToken();
-      effectRan.current = true;
-    }
-  }, []);
-
   return (
-    <main className="min-h-screen bg-gradient-to-br from-gray-950 via-gray-900 to-black flex items-center justify-center px-6">
-      <div className="w-full max-w-md">
-        {showSignUp ? (
-          <SignUp setShowSignUp={setShowSignUp} setShowSignIn={setShowSignIn} />
-        ) : (
-          <SignIn setShowSignUp={setShowSignUp} setShowSignIn={setShowSignIn} />
-        )}
-      </div>
-    </main>
+    <div className="min-h-screen bg-gradient-to-br from-black via-slate-950 to-slate-900 max-h-auto"></div>
   );
 }
