0707f75dbb0f5fd93b1caf1f53efe5b84a0ae154
Hairum Qureshi <hqureshi@udel.edu>
2026-06-05 20:27:56 -0400

    made homepage mobile responsive

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,13 +1,12 @@
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-black via-slate-950 to-slate-900 max-h-auto">
-      <div className="absolute w-full rounded-md ml-auto mr-auto left-0 right-0 top-1/3 -translate-y-1/2 p-10 text-center">
+      <div className="absolute w-full rounded-md ml-auto mr-auto left-0 right-0 top-1/2 lg:top-1/3 -translate-y-1/2 p-10 text-center">
         <div className="text-white space-y-6">
           <h1 className="font-bold text-6xl">Share Your Projects.</h1>
           <h1 className="font-semibold text-5xl">
             Find Your Next Inspiration.
           </h1>
-
           <p className="text-gray-400 text-lg w-1/2 text-center mx-auto">
             Developer Showcase is the community for showcasing and exploring
             developer projects, complete with GitHub integration. Start sharing
